9b3e8f95a12eb106ee6125b53d7731464f2ea4fd
felix amoquandoh <55555068+paakowamoquandoh@users.noreply.github.com>
2024-07-16 10:21:28 +0000

    app files updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import React, { useRef } from "react";
 import { Container, Row, Col } from "reactstrap";
 import { Link, NavLink } from "react-router-dom";
 import "../../styles/header.css";
+import logo from "../../assets/media/logo.jpeg"
 
 const navLinks = [
   {
@@ -43,7 +44,7 @@ const Header = () => {
               <div className="header__top__left">
                 <span>Need Help?</span>
                 <span className="header__top__help">
-                  <i class="ri-phone-fill"></i> +1-202-555-0149
+                  <i class="ri-phone-fill"></i> +233 240 234 587
                 </span>
               </div>
             </Col>
@@ -71,9 +72,9 @@ const Header = () => {
               <div className="logo">
                 <h1>
                   <Link to="/home" className=" d-flex align-items-center gap-2">
-                    <i class="ri-car-line"></i>
-                    <span>
-                      Rent Car <br /> Service
+                    {/* <i class="ri-car-line"></i> */}
+                    <span className="brandLogo">
+                     <img src={logo} alt=""/>
                     </span>
                   </Link>
                 </h1>
@@ -81,25 +82,24 @@ const Header = () => {
             </Col>
 
             <Col lg="3" md="3" sm="4">
-              <div className="header__location d-flex align-items-center gap-2">
+              <div className="header__location align-items-center">
                 <span>
                   <i class="ri-earth-line"></i>
                 </span>
                 <div className="header__location-content">
-                  <h4>Bangladesh</h4>
-                  <h6>Sylhet City, Bangladesh</h6>
+                  <h4>Accra/Tema</h4>
                 </div>
               </div>
             </Col>
 
             <Col lg="3" md="3" sm="4">
-              <div className="header__location d-flex align-items-center gap-2">
+              <div className="header__location align-items-center">
                 <span>
                   <i class="ri-time-line"></i>
                 </span>
                 <div className="header__location-content">
-                  <h4>Sunday to Friday</h4>
-                  <h6>10am - 7pm</h6>
+                  <h4>24Hours</h4>
+                 
                 </div>
               </div>
             </Col>
@@ -110,7 +110,7 @@ const Header = () => {
               sm="0"
               className=" d-flex align-items-center justify-content-end "
             >
-              <button className="header__btn btn ">
+              <button className="header__btn btn">
                 <Link to="/contact">
                   <i class="ri-phone-line"></i> Request a call
                 </Link>
@@ -128,6 +128,10 @@ const Header = () => {
             <span className="mobile__menu">
               <i class="ri-menu-line" onClick={toggleMenu}></i>
             </span>
+
+            <span className="brandLogoMobile">
+                     <img src={logo} alt=""/>
+            </span>            
 
             <div className="navigation" ref={menuRef} onClick={toggleMenu}>
               <div className="menu">
