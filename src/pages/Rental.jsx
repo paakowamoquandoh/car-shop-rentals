cdd4a4f7b7997d6ead18cd8a44ec9b7ae388a1ea
felix amoquandoh <55555068+paakowamoquandoh@users.noreply.github.com>
2024-10-28 12:36:21 +0000

    app file modified

--- src/pages/Rental.jsx
+++ src/pages/Rental.jsx
@@ -56,7 +56,6 @@ const Rental = () => {
         <Container>
           <Row>
             <Col lg="12" className="text-center mb-5">
-              <h6 className="section__subtitle">Come with</h6>
               <h2 className="section__title">Hot Offers</h2>
             </Col>
 
