7ca1799f8c02bd2b8714226b42571a1320987bc7
felix amoquandoh <55555068+paakowamoquandoh@users.noreply.github.com>
2024-11-04 22:16:00 +0000

    ap files update

--- api/index.js
+++ api/index.js
@@ -34,7 +34,7 @@ app.use(cors({
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
-    }
+    } 
   },
   credentials: true,
 }));
--- src/components/UI/PaymentMethod.jsx
+++ src/components/UI/PaymentMethod.jsx
@@ -7,33 +7,26 @@ import "../../styles/payment-method.css";
 const PaymentMethod = () => {
   return (
     <>
-      <div className="payment">
+      {/* <div className="payment">
         <label htmlFor="" className="d-flex align-items-center gap-2">
           <input type="radio" /> Direct Bank Transfer
         </label>
-      </div>
+      </div> */}
 
       <div className="payment mt-3">
         <label htmlFor="" className="d-flex align-items-center gap-2">
-          <input type="radio" /> Cheque Payment
+          <input type="radio" /> Mobile Money 🇬🇭
         </label>
       </div>
 
       <div className="payment mt-3 d-flex align-items-center justify-content-between">
         <label htmlFor="" className="d-flex align-items-center gap-2">
-          <input type="radio" /> Master Card
+          <input type="radio" />Visa / Master Card
         </label>
 
         <img src={masterCard} alt="" />
       </div>
 
-      <div className="payment mt-3 d-flex align-items-center justify-content-between">
-        <label htmlFor="" className="d-flex align-items-center gap-2">
-          <input type="radio" /> Paypal
-        </label>
-
-        <img src={paypal} alt="" />
-      </div>
       <div className="payment text-end mt-5">
         <button>Reserve Now</button>
       </div>
